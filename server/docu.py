7aa1f1c1db25bef8e1867abbae4411d8d5faf987
WaterMinding <1271306815@qq.com>
2024-12-18 09:17:38 +0800

    feat(docu.py): 数据层新增图像块
        数据层编写了图像块，使用matplotlib绘图，暂不支持图像交互。
    
    docs(后端开发逻辑.md): 修改文本块和图像块内容
    
        文本块和图像块的实现与计划有出入，故修改了这两部分的文档。

--- server/docu.py
+++ server/docu.py
@@ -1,6 +1,8 @@
 # 导包
 import os
 import time
+import base64
+from io import BytesIO
 from exception import ParamTypeException
 from exception import PathNotExistsException
 from exception import RedundantOperationException
@@ -77,9 +79,6 @@ class TextCell(Cell):
     
 
     # 导出文本方法
-    '''
-        此处path必须是原始字符串且 不 以 '\' 结尾
-    '''
     def export_txt(self,path:str):
 
         # 检查参数类型
@@ -93,3 +92,74 @@ class TextCell(Cell):
         # 创建、打开、编辑并保存txt文件
         with open(path + self.get_cell_id() + '.txt','w') as file:
             file.write(self.get_text())
+
+
+# 定义图表块类
+class ChartCell(Cell):
+
+    # 构造方法
+    def __init__(self, docu_name:str):
+
+        # 检查参数类型
+        type_check(docu_name,'docu_name',str)
+
+        cell_id = docu_name + 'Chart' + str(time.time())
+        cell_type = 'Chart'
+
+        # 构造父类对象
+        super().__init__(cell_id,cell_type)
+
+        # 定义图表块属性：figure对象
+        self.__figure = None
+    
+
+    # 注入figure对象方法
+    def set_figure(self,plt_figure):
+
+        # 检查是否已经存在figure对象
+        if self.__figure == None:
+            
+            # 注入对象
+            self.__figure = plt_figure
+
+        else:
+            raise RedundantOperationException('重复注入figure对象')
+
+    
+    # 返回figure对象方法
+    def get_figure(self):
+
+        return self.__figure
+    
+
+    # 返回图像编码
+    def get_base64(self):
+
+        # 图像保存至缓冲区
+        buf = BytesIO()
+        self.__figure.savefig(buf,format = 'png')
+        buf.seek(0)
+
+        # 图像base64编码
+        image_base64 = base64.b64encode(buf.read()).decode('utf-8')
+
+        # 包装为URL
+        src = f"data:image/png;base64,{image_base64}"
+
+        return src
+    
+    
+    # 保存图像方法
+    def save_figure(self,path:str):
+
+        # 检查参数类型
+        type_check(path,'path',str)
+
+        # 检查与修饰路径
+        if not os.path.exists(path):
+            raise PathNotExistsException('导出路径不存在')
+        path = path + '\\'
+
+        # 保存图像
+        self.__figure.savefig(path + self.get_cell_id() + '.png')
+
