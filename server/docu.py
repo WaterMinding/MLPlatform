cb6c981f57c4204d460559c78c3a4406ef3dc228
WaterMinding <1271306815@qq.com>
2024-12-31 18:52:28 +0800

    feat(docu.py): 数据层新增类与原有类改动
        新增数据块类、算子块类。
    
        块父类增加属性：元数据字典。
        块父类增加元数据词典的覆盖、增、删、查、改方法。
    
        图像块类、文本块类删除与块父类重叠方法。
    
    docs: 更改数据层规划
    
        为数据块类增加了返回DataFrame对象的复制体的方法。

--- server/docu.py
+++ server/docu.py
@@ -32,15 +32,45 @@ class Cell:
         # 定义块属性：块类型
         self.__type = cell_type
 
+        # 定义块属性：元数据词典
+        self.__meta_dict = {}
 
     # 返回块ID方法
     def get_cell_id(self) -> str:
         return self.__id
 
-
     # 返回块类型方法
     def get_cell_type(self) -> str:
         return self.__type
+    
+    # 覆盖元数据词典方法
+    def set_meta(self,new_meta_dict):
+
+        self.__meta_dict = new_meta_dict
+    
+    # 元数据词典新增数据方法
+    def add_meta(self,additive_meta_dict):
+
+        self.__meta_dict.update(additive_meta_dict)
+    
+    # 元数据词典删除数据方法
+    def delete_meta(self,delete_key):
+
+        if delete_key in self.__meta_dict.keys():
+            self.__meta_dict.pop(delete_key)
+    
+    # 查询元数据方法
+    def query_meta(self,query_key):
+
+        if query_key in self.__meta_dict.keys():
+            return self.__meta_dict[query_key]
+        else:
+            return None
+
+    # 返回元数据词典方法
+    def get_meta(self):
+
+        return self.__meta_dict
 
 
 # 定义文本块类（继承Cell类）
@@ -60,41 +90,26 @@ class TextCell(Cell):
 
         # 定义文本块属性：文本字符串
         self.__text = None
-    
 
-    # 更新文本字符串方法
+        # 删除元数据字典属性
+        self.__meta_dict = None
+    
+    # 覆盖文本字符串方法
     def set_text(self,new_text:str):
 
         # 检查参数类型
         type_check(new_text,'new_text',str)
 
-        # 更新文本字符串
+        # 覆盖文本字符串
         self.__text = new_text
     
-
     # 返回文本字符串方法
     def get_text(self) -> str:
 
         return self.__text
-    
-
-    # 导出文本方法
-    def export_txt(self,path:str):
-
-        # 检查参数类型
-        type_check(path,'path',str)
-
-        # 检查与修饰路径
-        if not os.path.exists(path):
-            raise PathNotExistsException('导出路径不存在')
-        path = path + '\\'
-
-        # 创建、打开、编辑并保存txt文件
-        with open(path + self.get_cell_id() + '.txt','w') as file:
-            file.write(self.get_text())
 
 
-# 定义图表块类
+# 定义图像块类
 class ChartCell(Cell):
 
     # 构造方法
@@ -109,57 +124,54 @@ class ChartCell(Cell):
         # 构造父类对象
         super().__init__(cell_id,cell_type)
 
-        # 定义图表块属性：figure对象
-        self.__figure = None
-    
 
-    # 注入figure对象方法
-    def set_figure(self,plt_figure):
+# 定义算子块类
+class OperatorCell(Cell):
 
-        # 检查是否已经存在figure对象
-        if self.__figure == None:
-            
-            # 注入对象
-            self.__figure = plt_figure
-
-        else:
-            raise RedundantOperationException('重复注入figure对象')
-
-    
-    # 返回figure对象方法
-    def get_figure(self):
-
-        return self.__figure
-    
-
-    # 返回图像编码
-    def get_base64(self):
-
-        # 图像保存至缓冲区
-        buf = BytesIO()
-        self.__figure.savefig(buf,format = 'png')
-        buf.seek(0)
-
-        # 图像base64编码
-        image_base64 = base64.b64encode(buf.read()).decode('utf-8')
-
-        # 包装为URL
-        src = f"data:image/png;base64,{image_base64}"
-
-        return src
-    
-    
-    # 保存图像方法
-    def save_figure(self,path:str):
+    # 构造方法
+    def __init__(self, docu_name:str):
 
         # 检查参数类型
-        type_check(path,'path',str)
+        type_check(docu_name,'docu_name',str)
 
-        # 检查与修饰路径
-        if not os.path.exists(path):
-            raise PathNotExistsException('导出路径不存在')
-        path = path + '\\'
+        cell_id = docu_name + 'Operator' + str(time.time())
+        cell_type = 'Operator'
 
-        # 保存图像
-        self.__figure.savefig(path + self.get_cell_id() + '.png')
+        # 构造父类对象
+        super().__init__(cell_id,cell_type)
+
+
+# 定义数据块类
+class OperatorCell(Cell):
+
+    # 构造方法
+    def __init__(self, docu_name:str):
+
+        # 检查参数类型
+        type_check(docu_name,'docu_name',str)
+
+        cell_id = docu_name + 'Data' + str(time.time())
+        cell_type = 'Data'
+
+        # 构造父类对象
+        super().__init__(cell_id,cell_type)
+
+        # 定义数据块属性：Dataframe对象引用
+        self.__df = None
+    
+    # 覆盖DataFrame引用方法
+    def set_df(self,new_df):
+
+        self.__df = new_df
+    
+    # 返回DataFrame引用方法
+    def get_df_pointer(self):
+
+        return self.__df
+    
+    # 获取DataFrame对象的复制体
+    def get_df_copy(self):
+
+        return self.__df.copy()
+    
 
