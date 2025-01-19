3405025cc58b72228487c2d5715fa2119109e8cf
WaterMinding <1271306815@qq.com>
2025-01-19 14:42:55 +0800

    feat(main.py): 完成两个测试接口
        完成了两个暂时用来测试的接口。
    
    refractor: 整理目录内容
    
        整理了文件夹目录内容

--- server/main.py
+++ server/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from docu import Docu
@@ -9,14 +10,44 @@ app = FastAPI()
 document = None
  
 # 配置 Jinja2 模板目录
-templates = Jinja2Templates(directory="../templates")
+templates = Jinja2Templates(directory = "templates")
  
 # 主界面接口
-@app.get("/")
-async def read_root(request: Request):
+@app.get(path = "/", tags = ['获取主界面'])
+async def read_root(request:Request):
 
     context = {"request": request}
 
     return templates.TemplateResponse("main.html", context)
 
-# 
+@app.post(path = '/docu', tags = ['新建文档'])
+async def create_docu(name:str):
+
+    global document
+    document = Docu(name)
+
+    return{
+        "docu_id":document.get_docu_id()
+    }
+
+@app.get(path = '/docu/id', tags = ['获取文档名称'])
+async def get_docu_name():
+
+    global document
+    if document != None:
+
+        return{
+            "docu_id":document.get_docu_id()
+        }
+    
+    else:
+
+        return{
+            "error":"当前无文档"
+        }
+
+
+
+if __name__ == "__main__":
+
+    uvicorn.run("main:app",port = 8168,reload = True)
