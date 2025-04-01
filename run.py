d63e1d35a36ddc6423caae42d6cbe7f1b00cdb00
WaterMinding <1271306815@qq.com>
2025-04-01 13:33:07 +0800

    feat(run.py): 为启动脚本增加了命令行参数
        为 run.py 增加了命令行参数，具体如下：
    
            -H --host 设定服务部署的 IP 。
            -p --port 设定服务监听的端口。
            -r --reload 设定服务是否在代码修改时重载。

--- run.py
+++ run.py
@@ -1,9 +1,42 @@
 import uvicorn
+import argparse
+
+
+argparse = argparse.ArgumentParser(
+    description="DataCell Server"
+)
+
+argparse.add_argument(
+    '-H', '--host',
+    type=str,
+    default='127.0.0.1',
+    help='Server host'
+)
+
+argparse.add_argument(
+    '-p', '--port',
+    type=int,
+    default=8168,
+    help='Server port'
+)
+
+argparse.add_argument(
+    '-r', '--reload',
+    type=bool,
+    default=False,
+    help='Server reload'
+)
+
+args = argparse.parse_args()
+
 
 if __name__ == '__main__':
     
+    print(args.reload)
+
     uvicorn.run(
         'app.main:app', 
-        port=8168, 
-        reload=True
+        host = args.host,
+        port = args.port, 
+        reload = args.reload,
     )
